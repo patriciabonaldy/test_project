49f1c1106212403b5886ab81436ef89c61712815
Patricia Bonaldy <bonaldypatricia@gmail.com>
2020-07-07 22:36:25 -0300

    Add files via upload

---
+++ beers-cli/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/patriciabonaldy/test_project"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.Execute()
+}
